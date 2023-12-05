4e28e38d2680ad26b2ba9e3aa07cf9f6993e6338
alenaVSk <elenavsk.7@gmail.com>
2023-12-05 23:29:44 +0300

    3

--- drfsite/delicious_way/views.py
+++ drfsite/delicious_way/views.py
@@ -1,9 +1,26 @@
+from django.forms.models import model_to_dict
 from rest_framework import generics
 from .models import DeliciousWay
 from django.shortcuts import render
 from .serializer import DeliciousWaySerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
-class DeliciousWayAPIView(generics.ListAPIView):
-    queryset = DeliciousWay.objects.all()
-    serializer_class = DeliciousWaySerializer
+class DeliciousWayAPIView(APIView):
+    def get(self, request):
+        lst = DeliciousWay.objects.all().values()
+        return Response({'posts': list(lst)})
+
+    def post(self, request):
+        post_new = DeliciousWay.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+
+        return Response({'post': model_to_dict(post_new)})
+
+# class DeliciousWayAPIView(generics.ListAPIView):
+#     queryset = DeliciousWay.objects.all()
+#     serializer_class = DeliciousWaySerializer
