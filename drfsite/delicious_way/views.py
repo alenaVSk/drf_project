ced96ca5b5b5b9264e26030086ff2f4d64113655
alenaVSk <elenavsk.7@gmail.com>
2024-02-03 12:45:42 +0300

    authentication, pagination

--- drfsite/delicious_way/views.py
+++ drfsite/delicious_way/views.py
@@ -2,8 +2,10 @@ from django.forms.models import model_to_dict
 from rest_framework import generics, viewsets, mixins
 from .models import DeliciousWay
 from django.shortcuts import render
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
@@ -12,16 +14,24 @@ from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from .serializer import DeliciousWaySerializer
 
 
+class DeliciousWayAPIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
+
+
 class DeliciousWayAPIList(generics.ListCreateAPIView):
     queryset = DeliciousWay.objects.all()
     serializer_class = DeliciousWaySerializer
-    #permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = DeliciousWayAPIListPagination
 
 
 class DeliciousWayAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = DeliciousWay.objects.all()
     serializer_class = DeliciousWaySerializer
-    permission_classes = (IsOwnerOrReadOnly,)
+    # authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
 
 class DeliciousWayAPIDestroy(generics.RetrieveDestroyAPIView):
--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from delicious_way.views import *
 from rest_framework import routers
@@ -24,7 +25,13 @@ from rest_framework import routers
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/delisious_way/', DeliciousWayAPIList.as_view()),
+    path('api/v1/drf-auth/', include('rest_framework.urls')),
     path('api/v1/delisious_way/<int:pk>/', DeliciousWayAPIUpdate.as_view()),
     path('api/v1/delisious_waydelete/<int:pk>/', DeliciousWayAPIDestroy.as_view()),
+    path('api/v1/auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
